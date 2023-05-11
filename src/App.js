0eb28e2cead2d6d68378f07ec1e21a41941907d9
Grover Cristobal <elvis.cristobal.garrido@gmail.com>
2023-05-11 12:41:03 -0500

    5. Crear componentes: comopnente Botón

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
                 src={freeCodeCampLogo} 
                 alt='Logo de freecodecamp' />
             </div>
+            {/* Una alternativa para crear los componentes es crear un componente completo para la Calculadora y luego crear los componentes más pequeños, pero en este caso
+            vamos a crear los componentes pequeños como botones y pantalla y los vamos a ubicar aqui en el componente principal de la Aplicación en lugar de crear otro 
+            componente de la Calculadora */}
             <div className='contenedor-calculadora'>
                 {/* 4 filas para los números y signos */}
                 <div className='fila'></div>
