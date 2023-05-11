61127cefe7de7c130c6486b63189806416d44205
Grover Cristobal <elvis.cristobal.garrido@gmail.com>
2023-05-11 11:50:27 -0500

    2. Comenzar el Proyecto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import freeCodeCampLogo from './images/freecodecamp-logo.png'
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+        <div className='App'>
+            <div className='freecodecamp-logo-contenedor'>
+                <img 
+                className='freecodecamp-logo' 
+                src={freeCodeCampLogo} 
+                alt='Logo de freecodecamp' />
+            </div>
+        </div>
+    );
 }
 
 export default App;
