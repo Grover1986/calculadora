04cff9433f768bd2d94ff776b6bc1024574a93d0
Grover Cristobal <elvis.cristobal.garrido@gmail.com>
2023-05-11 20:12:09 -0500

    10. Crear los Botones

--- src/App.js
+++ src/App.js
@@ -18,11 +18,28 @@ function App() {
                 {/* 4 filas para los números y signos */}
                 <div className='fila'>
                     <Boton>1</Boton>
+                    <Boton>2</Boton>
+                    <Boton>3</Boton>
                     <Boton>+</Boton>
                 </div>
-                <div className='fila'></div>
-                <div className='fila'></div>
-                <div className='fila'></div>
+                <div className='fila'>
+                    <Boton>4</Boton>
+                    <Boton>5</Boton>
+                    <Boton>6</Boton>
+                    <Boton>-</Boton>
+                </div>
+                <div className='fila'>
+                    <Boton>7</Boton>
+                    <Boton>8</Boton>
+                    <Boton>8</Boton>
+                    <Boton>*</Boton>
+                </div>
+                <div className='fila'>
+                    <Boton>=</Boton>
+                    <Boton>0</Boton>
+                    <Boton>.</Boton>
+                    <Boton>/</Boton>
+                </div>
                 {/* fila para el botón Limpiar */}
                 <div className='fila'></div>
             </div>
